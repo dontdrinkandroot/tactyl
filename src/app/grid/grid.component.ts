b4ce52d8e16742a0b2bc13f4bfb0d2899929a518
Philip Washington Sorst <philip@sorst.net>
2025-04-29 12:35:42 +0200

    feat(grid): implement dynamic grid layout with note buttons and customizable rows/columns

--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -1,16 +1,38 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
-import {ControlButtonComponent} from "../button/control-button.component";
+import {ChangeDetectionStrategy, Component, HostBinding} from '@angular/core';
 import {NoteButtonComponent} from "../button/note-button.component";
 
 @Component({
     selector: 'app-grid',
     imports: [
-        ControlButtonComponent,
         NoteButtonComponent
     ],
     templateUrl: './grid.component.html',
-    changeDetection: ChangeDetectionStrategy.OnPush
+    styleUrl: './grid.component.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class GridComponent {
 
+    protected cols: number[] = Array.from(Array(16).keys());
+
+    protected rows: number[] = Array.from(Array(16).keys());
+
+    @HostBinding('style.grid-template-columns')
+    protected get gridTemplateColumns(): string {
+        return `repeat(${this.cols.length + 1}, 1fr)`;
+    }
+
+    @HostBinding('style.grid-template-rows')
+    protected get gridTemplateRows(): string {
+        return `repeat(${this.rows.length + 2}, 1fr)`;
+    }
+
+    protected getChanel(row: number, col: number): number {
+        const idx = col + (row * 32);
+        return Math.floor(idx / 128);
+    }
+
+    protected getNote(row: number, col: number): number {
+        const idx = col + (row * 32);
+        return idx % 128;
+    }
 }
